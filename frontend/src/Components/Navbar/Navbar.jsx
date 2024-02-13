86b7494f2251a23787f8e688e1b4dae2637044b2
Siva Kishore Reddy <55317226+sivakishorereddyofficial@users.noreply.github.com>
2024-02-13 16:21:42 -0600

    Add files via upload

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+import  logo from '../Assets/logo.png'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        
+    </div>
+  )
+}
+
+export default Navbar
